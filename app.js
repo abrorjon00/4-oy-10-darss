639fd10c8700cd1c7eb4166f559bb2bd3d6431b4
abdurahimov0101 <abrorjonabdurahimov42@gmail.com>
2024-10-11 13:04:07 +0300

    darss

---
+++ app.js
@@ -0,0 +1,212 @@
+// sodda masalalar
+
+// 1-misol
+
+// LENGTH OID MASALALAR
+
+// let arr = [2, 3, 5, 7, 9, 2, 5, 3];
+// console.log(arr.length);
+
+// 2-misol
+// let arr = [2, '', true, false, ``];
+// console.log(arr.length);
+
+// 3-misol
+// let arr = [2, 3, 5, 7, 9, 2, 5, 3];
+
+//     console.log(arr.length);
+
+
+// Concat oid masalalar
+
+// 1-misol
+
+// let arr1 = [1, 2, 3];
+// let arr2 = [4, 5, 6];
+// let res = arr1.concat(arr2)
+// console.log(res);
+
+// 2- misol
+
+// let arr1 = ['salom'];
+// let str2 = ['have you'];
+// let str3 = ['goodbye'];
+// let res = arr1.concat(str2, str3)
+// console.log(res);
+
+// 3-misol
+
+// let arr1 = [2, 3, 4];
+// let arr2 = [5, 6, 7];
+// let res = arr1.concat(arr2);
+// res.push(1000)
+// console.log(res);
+
+
+// Includes oid masalalar
+
+// 1-masala
+
+// let arr = [2, 3, 5];
+// console.log(arr.includes(5));
+
+// 3-misol
+
+// let arr = [2, 23, 43];
+ 
+//     console.log(arr.includes(10));
+
+
+
+// IndexOf, LastIndexOf 
+
+
+
+// 1-misol
+
+// let arr = [1, 2, 3, 2, 4];
+// let index = arr.indexOf(2);index
+// console.log(index);
+
+
+// 2-misol
+
+// let arr = [1, 2, 3, 2, 4];
+// let index = arr.lastIndexOf(2);index
+// console.log(index);
+
+// 3-misol
+
+// let arr = ['a', 'b', 'a'];
+// let index = arr.indexOf("a");
+// let lastindex = arr.lastIndexOf('a')
+// console.log(index, lastindex);
+
+
+
+//  Sort id masalalar
+
+// 1-misol
+
+// let arr = [2, 0, 2, 1, 4 ,3];
+// let res = arr.sort(function(a, b) {
+//     return a - b
+// })
+
+// console.log(res);
+
+// 2-misol
+
+// let words = ["olma", "banan", "anor", "shaftoli"];
+// words.sort();
+// console.log(words);
+
+// 3-misol
+
+// let arr = [2, 0, 2, 1, 4 ,3];
+// let res = arr.sort(function(a, b) {
+//     return b - a
+// })
+
+// console.log(res);
+
+
+// Reveres iod masalalar
+
+// 1-misol
+
+// let arr = [2, 0, 2, 1, 4 ,3];
+// arr.reverse()
+// console.log(arr);
+
+// 2-misol
+
+// let words = ["olma", "banan", "anor", "shaftoli"];
+// words.reverse()
+// console.log(words);
+
+// 3-misol
+
+// let arr = [2, 0, 2, 1, 4 ,3];
+// let res = arr.sort(function(a, b) {
+//     return a - b
+// })
+// res.reverse()
+// console.log(res);
+
+
+// toString oid masalalar
+
+// 1-misol
+// let arr1 = [1, 2, 3];
+// console.log(arr1.toString());
+
+// 2-misol
+
+// let arr = ['olma', 'banan', 'shaftoli'];
+// console.log(arr.toString()); 
+
+// 3-misol
+
+// let arr3 = [10, 20, 30];
+// console.log(arr3.toString());
+
+// Join iod masalalar
+
+// 1-misol\
+
+// let arr = ['olma', 'banan', 'shaftoli'];
+// console.log(arr.join('-')); 
+
+// 2-misol
+
+// let arr = ['Salom', 'dunyo'];
+// console.log(arr.join(' ')); 
+
+// 3-misol
+
+// let arr = [1, 2, 3];
+// console.log(arr.join('|')); 
+
+// Push Shift iod masalalar
+
+// 1-misol
+
+// let arr = [1, 2, 3];
+// arr.push(4);
+// console.log(arr);
+
+// 2-misol
+
+// let arr = [1, 2, 3];
+// arr.shift();
+// console.log(arr); 
+
+// 3-misol
+
+// let arr = [1, 2, 3];
+// arr.push(4, 5);
+// arr.shift();
+// console.log(arr);  
+
+// Unshift Pop iod masalalar
+
+// 1-misol
+
+// let arr= [2, 3, 4];
+// arr.unshift(1);
+// console.log(arr);  
+
+// 2-misol
+// let arr = [1, 2, 3, 4];
+// arr.pop();
+// console.log(arr);  
+
+
+// 3-misol
+
+// let arr = [2, 3, 4];
+// arr.unshift(1);
+// arr.pop();
+// console.log(arr);
+
